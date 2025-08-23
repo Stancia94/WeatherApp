425d98bd719c4c8e309062e080c66038a95ddc6d
Stancia94 <staskobenko88@gmail.com>
2025-08-23 14:11:39 +0300

    fixed temperature display switch

--- weather-app/src/components/ForecastCard/ForecastCard.jsx
+++ weather-app/src/components/ForecastCard/ForecastCard.jsx
@@ -2,6 +2,7 @@ import styles from "./ForecastCard.module.css";
 import cloudRain from "../../assets/icons/cloud-rain.svg";
 import humidity from "../../assets/icons/droplet-purple.svg";
 import thermometer from "../../assets/icons/thermometer.svg";
+import { useEffect, useState } from "react";
 const dayNames = [
   "Sunday",
   "Monday",
@@ -12,25 +13,31 @@ const dayNames = [
   "Saturday",
 ];
 export default function ForecastCard({ data, tempView }) {
-  if (tempView == "F") {
-    data.temperature_2m = Math.trunc(data.temperature_2m * (9 / 5) + 32);
-  }
+  const { time, relative_humidity_2m } = data;
+  const [temperature, setTemperature] = useState(data.temperature_2m);
+  useEffect(() => {
+    if (tempView == "F") {
+      setTemperature(Math.trunc(data.temperature_2m * (9 / 5) + 32));
+    } else {
+      setTemperature(data.temperature_2m);
+    }
+  }, [tempView, data.temperature_2m]);
   return (
     <article className={styles.card}>
       <h3 className={styles.time}>
-        {dayNames[data.time.getDay()] + " " + data.time.getDate()}
+        {dayNames[time.getDay()] + " " + time.getDate()}
       </h3>
       <div className={styles.weather}>Raining</div>
       <img src={cloudRain} className={styles.icon}></img>
       <div className={styles.tempWrapper}>
         <img src={thermometer} alt="" />
         <div className={styles.temp}>
-          {data.temperature_2m} {tempView == "F" ? "F" : "C"}°
+          {temperature} {tempView == "F" ? "F" : "C"}°
         </div>
       </div>
       <div className={styles.humidityWrapper}>
         <img src={humidity} className={styles.humidityIcon} alt="" />
-        <div className={styles.humidity}>{data.relative_humidity_2m} %</div>
+        <div className={styles.humidity}>{relative_humidity_2m} %</div>
       </div>
     </article>
   );
